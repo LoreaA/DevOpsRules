102ba4f1df6c4fd95e55ce78d67a033124ba517a
LoreaA <100093458+LoreaA@users.noreply.github.com>
2023-07-05 09:32:45 +0000

    repair all

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -2,17 +2,6 @@
 
 set -e
 echo "executing $0 {"
-rm -rf DevOpsRules_webserver
-git clone https://github.com/LoreaA/DevOpsRules_webserver.git
-
-cd DevopsRules_webserver/
-cp ../index.html .
-git add --all
-git commit -m "$(date)"
-git push
-
-rm -rf DevOpsRules_webserver
-
 
 echo "done $0 }"
 
--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -1,1 +1,18 @@
+#! /usr/bin/env sh
+
+set -e
+echo "executing $0 {"
+rm -rf DevOpsRules_webserver
+git clone https://github.com/LoreaA/DevOpsRules_webserver.git
+
+cd DevopsRules_webserver/
+cp ../index.html .
+git add --all
+git commit -m "$(date)"
+git push
+
+rm -rf DevOpsRules_webserver
+
+
+echo "done $0 }"
 
