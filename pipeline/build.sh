4281bf165de197f5e3a2f3e05bf7813ff836a3cb
LoreaA <100093458+LoreaA@users.noreply.github.com>
2023-07-05 11:25:24 +0200

    add build.sh

--- pipeline/build.sh
+++ pipeline/build.sh
@@ -2,8 +2,19 @@
 
 set -e
 echo "executing $0 {"
+rm -rf DevOpsRules_webserver
+git clone https://github.com/LoreaA/DevOpsRules_webserver.git
+
+cd DevopsRules_webserver/
+cp ../index.html .
+git add --all
+git commit -m "$(date)"
+git push
+
+rm -rf DevOpsRules_webserver
 
 
+echo "done $0 }"
 
-echo "done $0 "
+
 
