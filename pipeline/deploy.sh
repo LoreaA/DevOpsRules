4748219d533fbc8541852d62729643c3463d4515
LoreaA <100093458+LoreaA@users.noreply.github.com>
2023-07-05 10:11:26 +0000

    fix deploy

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -6,6 +6,8 @@ rm -rf DevOpsRules_webserver
 git clone "https://$MACHINE_USER_TOKEN@github.com/LoreaA/DevOpsRules_webserver.git"
 
 cd DevOpsRules_webserver/
+git config user.email "user@machine.com"
+git config user.name "Machine User"
 cp ../index.html .
 git add --all
 git commit -m "$(date)" || true
